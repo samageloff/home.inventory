680071fbf9a8c8abf79f80b7c6171ee74f5debb9
HUGE | Sam Ageloff <samuelageloff@gmail.com>
2014-07-21 17:06:11 -0400

    moved   app.use(express.static(__dirname));

--- server.js
+++ server.js
@@ -15,11 +15,11 @@ app.configure(function() {
   app.set('port', process.env.PORT || 3001);
   app.set('views', __dirname + '/views');
   app.set('view engine', 'hjs');
+  app.use(express.static(__dirname));
   app.use(express.methodOverride());
   app.use(express.logger('dev'));
   app.use(express.favicon());
   app.use(express.bodyParser());
-  app.use(express.static(__dirname));
   app.use(app.router);
 });
 
