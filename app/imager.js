76d9ee25ba2e8cfa1b65c2ac8d7c9913cd2afc8a
HUGE | Sam Ageloff <samuelageloff@gmail.com>
2014-07-22 10:52:43 -0400

    adding imager

---
+++ app/imager.js
@@ -0,0 +1,38 @@
+module.exports = {
+  variants: {
+    items: {
+      // keepNames: true,
+      resize: {
+        large: '800',
+      },
+      resizeAndCrop: {
+        gallery: {
+          resize: '320',
+          crop: '320x320'
+        },
+        thumb: {
+          resize: '80',
+          crop: '80x80'
+        }
+      }
+    }
+  },
+
+  storage: {
+    Local: {
+      path: '/tmp',
+      mode: 0777
+    },
+    S3: {
+      key: 'AKIAJSIGDTCPT22D3NEA',
+      secret: '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',
+      bucket: 'belongings-global',
+      cdn: 'http://belongings-global.s3.amazonaws.com', // (optional)
+      storageClass: 'REDUCED_REDUNDANCY' // (optional)
+      // set `secure: false` if you want to use buckets with characters like '.' (dot)
+    }
+    // uploadDirectory: 'images/uploads/'
+  },
+
+  debug: true
+}
